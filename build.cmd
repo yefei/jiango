083febabcb52d84dfb1b268c28ff9e5a595fc936
Yefei <Yefei@46f8ed9e-7293-422d-b0c4-309f3a4ff27e>
2015-08-28 08:38:28 +0000

    init build

---
+++ build.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+rem --exclude-source-files
+python setup.py bdist_egg
---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('jiango'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+setup(
+    name = "jiango",
+    version = "0.1.0",
+    author = 'Yefei',
+    author_email = '316606233@qq.com',
+    url = 'http://djangobbs.com',
+    install_requires = ['django >=1.4,<1.5'],
+    package_data = {'':['*.*']},
+    packages = packages,
+    data_files = data_files
+)
