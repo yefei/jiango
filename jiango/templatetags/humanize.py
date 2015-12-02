8bb222a24c713c1ef9a73b9ac9cc181a1bf6c920
Yefei <Yefei@46f8ed9e-7293-422d-b0c4-309f3a4ff27e>
2015-12-02 08:51:22 +0000

    新增 templatetags/humanize.py   humanizesecond   naturalsecond   filesize   intcomma

---
+++ jiango/templatetags/humanize.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Created on 2015-12-2
+# @author: Yefei
+from django import template
+from jiango.utils.humanize import humanize_second, humanize_size
+
+
+register = template.Library()
+register.filter('humanizesecond', humanize_second, is_safe=True)
+
+
+@register.filter(is_safe=True)
+def naturalsecond(seconds):
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    return ''.join(((u'%d天' % days if days else ''),
+                    (u'%d小时' % hours if hours else ''),
+                    (u'%d分' % minutes if minutes else ''),
+                    (u'%d秒' % seconds if seconds else ''),
+                    ))
+
+
+@register.filter(is_safe=True)
+def filesize(value):
+    size, unit = humanize_size(int(value))
+    if unit is None:
+        return size
+    return '%.2f %s' %(size, unit)
+
+
+@register.filter(is_safe=True)
+def intcomma(value):
+    return '{:,}'.format(value)
