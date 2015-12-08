7a76371063d0da5557a0f5f463135638c4fe7006
Yefei <Yefei@46f8ed9e-7293-422d-b0c4-309f3a4ff27e>
2015-12-08 10:31:27 +0000

    jiango.forms.fields 新增 GroupedModelChoiceField

--- jiango/forms/fields.py
+++ jiango/forms/fields.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Created on 2015-12-2
 # @author: Yefei
-from django.forms import Field
+from itertools import groupby
+from django.forms import Field, ModelChoiceField
+from django.forms.models import ModelChoiceIterator
 from django.core.validators import EMPTY_VALUES
 from jiango.utils.humanize import parse_humanize_second
 from .widgets import SecondInput
@@ -19,3 +21,38 @@ class SecondField(Field):
         if value in EMPTY_VALUES:
             return ''
         return parse_humanize_second(value)
+
+
+class GroupedModelChoiceField(ModelChoiceField):
+    def __init__(self, queryset, group_by_field, group_label=None, *args, **kwargs):
+        super(GroupedModelChoiceField, self).__init__(queryset, *args, **kwargs)
+        self.group_by_field = group_by_field
+        if group_label is None:
+            self.group_label = lambda group: group
+        else:
+            self.group_label = group_label
+    
+    def _get_choices(self):
+        if hasattr(self, '_choices'):
+            return self._choices
+        return GroupedModelChoiceIterator(self)
+    choices = property(_get_choices, ModelChoiceField._set_choices)
+
+
+class GroupedModelChoiceIterator(ModelChoiceIterator):
+    def __iter__(self):
+        if self.field.empty_label is not None:
+            yield ('', self.field.empty_label)
+        if self.field.cache_choices:
+            if self.field.choice_cache is None:
+                self.field.choice_cache = [
+                    (self.field.group_label(group), [self.choice(ch) for ch in choices])
+                        for group,choices in groupby(self.queryset.all(),
+                            key=lambda row: getattr(row, self.field.group_by_field))
+                ]
+            for choice in self.field.choice_cache:
+                yield choice
+        else:
+            for group, choices in groupby(self.queryset.all(),
+                    key=lambda row: getattr(row, self.field.group_by_field)):
+                yield (self.field.group_label(group), [self.choice(ch) for ch in choices])
