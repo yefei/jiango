9dac3ee7d3e326a839c130b8c843d6a58ef26f22
YeFei <316606233@qq.com>
2019-04-11 23:38:58 +0800

    模板新增标签 verbatim 用于忽略 django 模板语法

--- jiango/templatetags/helper.py
+++ jiango/templatetags/helper.py
@@ -63,3 +63,31 @@ def startswith(v, prefix):
     if not v:
         return False
     return v.startswith(prefix)
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
